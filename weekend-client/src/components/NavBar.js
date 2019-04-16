9ecc6bb6be4b3914aebc7c04941b6766f14ec985
github username <github email address>
2019-04-16 13:05:18 -0700

    refactored navbar

--- weekend-client/src/components/NavBar.js
+++ weekend-client/src/components/NavBar.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
 const NavBar = (props) => {
-	let displayUser = () => {
-		if (props.currentUser) {
-			return (
-				<div className="nav-bar">
-					<div>{props.currentUser.firstName + ' ' + props.currentUser.lastName}</div>
-					<button className="button" id="logout-button" onClick={props.logoutUser}>
-						Logout
-					</button>
-				</div>
-			);
-		} else {
-			return null;
-		}
-	};
-
-	return displayUser();
+	return (
+		<div className="nav-bar">
+			<div>{props.currentUser.firstName + ' ' + props.currentUser.lastName}</div>
+			<button className="button" id="logout-button" onClick={props.logoutUser}>
+				Logout
+			</button>
+		</div>
+	);
 };
 
 export default NavBar;
