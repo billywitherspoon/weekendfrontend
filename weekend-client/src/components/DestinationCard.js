b68c36f60bc889079f8e456d89eea0494c1b3d36
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-04-17 12:58:58 -0700

    showing wekend weather partially done in destination card

--- weekend-client/src/components/DestinationCard.js
+++ weekend-client/src/components/DestinationCard.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 class DestinationCard extends Component {
+
+      determineDayOfWeek = () => {
+        let today = new Date()
+        let dayOfWeek = today.getDay()
+        this.howFarFromWeekend(dayOfWeek)
+      }
+
+      howFarFromWeekend = (dayOfWeek) => {
+        if (dayOfWeek === 6 || dayOfWeek === 7) {
+          console.log("it is the weekend. //Return current weather?")
+        }else{
+          let daysUntilWeekend = 5 - dayOfWeek
+          return daysUntilWeekend
+        }
+      }
+
+      showWeatherForWeekend = () => {
+        let daysUntilWeekend = this.determineDayOfWeek()
+        //determine appropriate number of days ahead to show forecast
+        //return the forecast for upcoming Fri/Sat/Sun
+        console.log("working on weather.")
+      }
+
   render() {
 
     let recentForecast = this.props.destination.forecasts
     return (
       <div  className="destination-card">
+        {this.determineDayOfWeek()}
         <h2>{this.props.destination.name}</h2>
+        <img src={this.props.destination.image} className="destination-card-image"/>
         <h5>{recentForecast[recentForecast.length -1].data.daily.summary}</h5>
         <p>{recentForecast[recentForecast.length -1].data.daily.icon}</p>
       </div>
