269b560d3cd5e2ea4c07d811ffe8f09497302957
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-04-17 18:06:15 -0700

    pre-merge

--- weekend-client/src/containers/DestinationFormContainer.js
+++ weekend-client/src/containers/DestinationFormContainer.js
@@ -62,6 +62,7 @@ class DestinationFormContainer extends Component {
 	};
 
 	setPlaceId = (placeId) => {
+		console.log("PLACE ID", placeId)
 		this.setState({ placeId });
 		// fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=photo&key=AIzaSyAntpQHNnQ1VhJKBJ8ikMKb7HZ-g83JxKA`)
 		fetch(
