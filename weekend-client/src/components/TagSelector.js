e1bb5a390973c9960ac6a77e4da9262776215e60
github username <github email address>
2019-04-18 11:11:11 -0700

    working on tag checkboxes

--- weekend-client/src/components/TagSelector.js
+++ weekend-client/src/components/TagSelector.js
@@ -4,7 +4,14 @@ const TagSelector = (props) => {
 	let currentUser = JSON.parse(sessionStorage.getItem('user'));
 
 	let renderCheckBoxes = () => {};
-	return <div>{renderCheckBoxes()}</div>;
+	render(<div id="check-box-container">{renderCheckBoxes()}</div>);
 };
+
+<form>
+	<div>
+		<h3>Filter By House</h3>
+	</div>
+	<input name="selectedHouse" type="radio" value="All" onChange={(ev) => props.handleFilterChange(ev)} />All
+</form>;
 
 export default TagSelector;
