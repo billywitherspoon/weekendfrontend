4b9786ccd9decc237998c070e41285c306eab996
github username <github email address>
2019-05-02 11:34:08 -0700

    removed css spinning logo

--- src/containers/ExploreContainer.js
+++ src/containers/ExploreContainer.js
@@ -15,12 +15,10 @@ class ExploreContainer extends Component {
 	};
 
 	fetchDestinations = () => {
-		fetch('https://fathomless-citadel-93230.herokuapp.com/api/v1/destinations')
-			.then((res) => res.json())
-			.then((json) => {
-				this.setState({ allDestinations: json });
-				console.log('all destinations', json);
-			});
+		fetch('https://localhost:3000/api/v1/destinations').then((res) => res.json()).then((json) => {
+			this.setState({ allDestinations: json });
+			console.log('all destinations', json);
+		});
 	};
 
 	renderDestinationList = () => {
